39862b9b6b6898b189f7f1e0ed6b8926298a5ed8
Luan <luan.b@stjamesperth.com.au>
2018-11-06 12:04:54 +0800

    installed styled componenets

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
+
+
+const CustomElement = styled.div`
+  color: green;
+  font-size: 30px;
+`
+
+const BlueElement = styled(CustomElement)`
+  color: blue;
+`
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <div>
+    <CustomElement>
+      hello
+    </CustomElement>
+    <BlueElement> hello blue</BlueElement>
+    </div>
+  );
   }
 }
 
