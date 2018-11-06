4481b59a46b1b4cfc5edecbda737ee1d04f5d3b1
Luan <luan.b@stjamesperth.com.au>
2018-11-06 14:37:05 +0800

    fixed up grid dispay colums

--- src/App.js
+++ src/App.js
@@ -3,25 +3,32 @@ import './App.css';
 import styled from 'styled-components'
 
 
-const CustomElement = styled.div`
+const Logo = styled.div`
   
-  font-size: 30px;
-`
+  font-size: 1.5em;
+`;
 
-const BlueElement = styled(CustomElement)`
+const ControlButton = styled.div`
   
-`
+`;
 
+const AppLayout = styled.div`
+  padding: 40px;
+  display: grid;
+  grid-template-columns: 180px auto 100px 100px;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div>
-    <CustomElement>
-      hello
-    </CustomElement>
-    <BlueElement> hello blue</BlueElement>
-    </div>
+      <AppLayout>
+    <Logo>
+      Crypto Dashboard
+    </Logo>
+    <div></div>
+    <ControlButton>Dashboard</ControlButton>
+    <ControlButton>Settings</ControlButton>
+  </AppLayout>
   );
   }
 }
